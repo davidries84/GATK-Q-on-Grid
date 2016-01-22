b2e691947007cd5ae81d295ef5713ed39da7de83
David Ries <david.ries84@googlemail.com>
2016-01-22 19:00:01 +0100

    some comments

--- run_GATK_pipeline.sh
+++ run_GATK_pipeline.sh
@@ -1,3 +1,7 @@
+# shell script for running the whole pipeline from bam files to quality filtered vcf files.
+# This is normaly what you want to do, because it is the easiest way. Input files and paths have to be set correctly, see README.
+
+# clear environment
 PATH=/vol/java-7/bin:/vol/python-2.7/bin:/vol/staden-2.0/bin:/vol/biotools/bin:/vol/gnu/bin:/vol/local/bin:/vol/X11/bin:/usr/bin:/bin:/usr/local/bin:/vol/codine-6.2/bin/lx24-amd64
 LIBATH=
 JAVA_HOME=
@@ -7,5 +11,6 @@ export LIBATH
 export JAVA_HOME
 
 
-
+# run Queue
+# parameters have to be set according to your files and system
 LD_LIBRARY_PATH=/vol/codine-6.2/lib/lx24-amd64/ java -d64 -Djava.library.path=/vol/codine-6.2/lib/lx24-amd64/ -Djava.io.tmpdir=/vol/codine-tmp -jar  /prj/gf-nugget/bin/Queue-3.2-2/Queue.jar   -S /prj/gf-nugget/scripts/GATK_pipeline.scala -R /prj/gf-nugget/processed-data/Assemblies/beet/RefBeet-1.2/RefBeet-1.2.joined.fa  -I red_merged.bam -I green_merged.bam  -jobRunner GridEngine   --job_parallel_env multislot  -jobResReq vf=5g -retry 1   -jobNative " -v PATH=/vol/biotools/bin:/vol/r-2.15/bin: -v  LD_LIBRARY_PATH=/vol/codine-6.2/lib/lx24-amd64/: -l arch=lx24-amd64"
